6ee157c9f4cd286de96acea93aaf3fe0ced1148a
pj0620 <pjm7748@g.rit.edu>
2022-05-06 18:18:35 -0700

    remove unneeded cors change

--- demo-api/index.js
+++ demo-api/index.js
@@ -7,33 +7,6 @@ const app = new express()
 app.use(express.json());
 app.use(cors());
 
-app.use(function(req, res, next) {
-  var oneof = false;
-  if(req.headers.origin) {
-      res.header('Access-Control-Allow-Origin', req.headers.origin);
-      oneof = true;
-  }
-  if(req.headers['access-control-request-method']) {
-      res.header('Access-Control-Allow-Methods', req.headers['access-control-request-method']);
-      oneof = true;
-  }
-  if(req.headers['access-control-request-headers']) {
-      res.header('Access-Control-Allow-Headers', req.headers['access-control-request-headers']);
-      oneof = true;
-  }
-  if(oneof) {
-      res.header('Access-Control-Max-Age', 60 * 60 * 24 * 365);
-  }
-
-  // intercept OPTIONS method
-  if (oneof && req.method == 'OPTIONS') {
-      res.send(200);
-  }
-  else {
-      next();
-  }
-});
-
 app.get('/api/supervisors', async (req, res, next) => {
   const supervisorResp = await axios.get('https://o3m5qixdng.execute-api.us-east-1.amazonaws.com/api/managers')
     .catch(e => {
